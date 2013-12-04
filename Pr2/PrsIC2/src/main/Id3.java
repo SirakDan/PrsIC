9ed8ce776818a3f68f98666b9dcfb65ce23df95b
dansirak <dansirak@gmail.com>
2013-12-04 12:26:12 +0100

    C 	Pr2/PrsIC2/juego1.txt

--- Pr2/PrsIC2/src/main/Id3.java
+++ Pr2/PrsIC2/src/main/Id3.java
@@ -3,25 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package main;
 
 import java.util.ArrayList;
+
 /**
  *
  * @author DanSirak
  */
 public class Id3 {
-    
+    public class Nodo {
+
+            private String nombre;
+            private String vertice;
+            private ArrayList<Nodo> hijos;
+
+            //Constructors
+            public Nodo() {
+                this.nombre = "";
+                this.hijos = new ArrayList();
+            }
+
+            public Nodo(String atributo) {
+                this.nombre = atributo;
+                this.hijos = new ArrayList();
+            }
+
+            public Nodo(String atributo, ArrayList<Nodo> nodos) {
+                this.nombre = atributo;
+                this.hijos = nodos;
+            }
+
+        }
     private class Id3Data {
+
+        public String atributo;
         public ArrayList<String> nombre;
         public int[] cuenta;
         public double[] p;
         public double[] n;
-        public int N;
+        public double N;
         public double[] r;
         public double merito;
+
         
+
         public Id3Data(int size) {
             this.nombre = new ArrayList();
             this.cuenta = new int[size];
@@ -30,51 +56,70 @@ public class Id3 {
             this.r = new double[size];
             this.merito = 0;
         }
-        
+
         public int isThere(String ejem) {
             boolean thereIs = false;
             int i = 0;
-            while (i < nombre.size() && !thereIs){
-                if (nombre.get(i).equalsIgnoreCase(ejem)) thereIs=true;
-                else i++;
+            while (i < nombre.size() && !thereIs) {
+                if (nombre.get(i).equalsIgnoreCase(ejem)) {
+                    thereIs = true;
+                } else {
+                    i++;
+                }
             }
-            if (!thereIs) i = -1; 
+            if (!thereIs) {
+                i = -1;
+            }
             return i;
         }
-        
+
         public double infor(double p, double n) {
-            return -p*Math.log(p)-n*Math.log(n);
+            double sol1, sol2;
+            if (p == 0) {
+                sol1 = 0;
+            } else {
+                sol1 = Math.log(p) / Math.log(2);
+            }
+            if (n == 0) {
+                sol2 = 0;
+            } else {
+                sol2 = Math.log(n) / Math.log(2);
+            }
+            return ((-1 * p) * sol1) - (n * sol2);
+
         }
-        
+
         public void ajusta() {
             for (int i = 0; i < nombre.size(); i++) {
-                p[i] = p[i]/cuenta[i];
-                n[i] = n[i]/cuenta[i];
-                r[i] = cuenta[i]/N;
-                merito += r[i]*infor(p[i],n[i]);
+                p[i] = p[i] / cuenta[i];
+                n[i] = n[i] / cuenta[i];
+                r[i] = cuenta[i] / N;
+                merito += r[i] * infor(p[i], n[i]);
+
             }
         }
     }
-    
+
     private ArrayList<String> listEjemplos;
     private ArrayList<String> listAtributos;
     private Id3Data id3data;
-    
-    
+
     public Id3() {
-	this.listEjemplos = new ArrayList();
-	this.listAtributos = new ArrayList();
+        this.listEjemplos = new ArrayList();
+        this.listAtributos = new ArrayList();
     }
 
     public Id3(ArrayList<String> listEjemplos, ArrayList<String> listAtributos) {
         this.listAtributos = listAtributos;
         this.listEjemplos = listEjemplos;
     }
+
     /**
      * Devuelve 1 si todos son "+", -1 si todos sin "-" y 0 si hay de ambos.
+     *
      * @param listEjemplos
      * @param listAtributos
-     * @return 
+     * @return
      */
     public int isItPlusOrMinus(ArrayList<String> listEjemplos) {
         String frase;
@@ -83,30 +128,36 @@ public class Id3 {
         for (int i = 0; i < listEjemplos.size(); i++) {
             frase = listEjemplos.get(i);
             split = frase.split(",");
-            if (split[split.length-1].equalsIgnoreCase("+")) plus++;
-            else if (split[split.length-1].equalsIgnoreCase("-")) minus++;
+            if (split[split.length - 1].equalsIgnoreCase("si")) {
+                plus++;
+            } else if (split[split.length - 1].equalsIgnoreCase("no")) {
+                minus++;
+            }
         }
-        if (plus != 0 && minus == 0) retorno = 1;
-        else if (plus == 0 && minus != 0) retorno = -1;
-        else retorno = 0;
+        if (plus != 0 && minus == 0) {
+            retorno = 1;
+        } else if (plus == 0 && minus != 0) {
+            retorno = -1;
+        } else {
+            retorno = 0;
+        }
         return retorno;
     }
-    
+
     /**
      * Devuelve la cantidad de información de un conjunto de datos.
+     *
      * @param p: Porcentaje de ejemplos positivos
      * @param n: Porcentaje de ejemplos negativos
-     * @return 
+     * @return
      */
-
-    
-    public String minMerito(ArrayList<String> listEjemplos, ArrayList<String> listAtributos){
-        double tmpMerito, minMerito=Double.MAX_VALUE;
+    public Id3Data minMerito(ArrayList<String> listEjemplos, ArrayList<String> listAtributos) {
+        double tmpMerito, minMerito = 10000;
         String candidato = "";
         int colocados, pos;
         Id3Data ejemplos;
         String[] frase;
-        for (int i = 0; i < listAtributos.size(); i++) {
+        for (int i = 0; i < listAtributos.size() - 1; i++) {
             ejemplos = new Id3Data(listEjemplos.size());
             ejemplos.N = listEjemplos.size();
             //Miramos a ver cuántos ejemplos diferentes de cada atributo hay
@@ -114,64 +165,98 @@ public class Id3 {
                 frase = listEjemplos.get(j).split(",");
                 pos = ejemplos.isThere(frase[i]);
                 if (pos == -1) {
-                    ejemplos.nombre.add(frase[i]);
                     pos = ejemplos.nombre.size();
+                    ejemplos.nombre.add(frase[i]);
                     ejemplos.cuenta[pos] = 1;
                     ejemplos.p[pos] = 0;
                     ejemplos.n[pos] = 0;
                 } else {
                     ejemplos.cuenta[pos]++;
                 }
-                if (frase[listAtributos.size()-1].equalsIgnoreCase("+")) ejemplos.p[pos]++;
-                else ejemplos.n[pos]++;
+                if (frase[listAtributos.size() - 1].equalsIgnoreCase("si")) {
+                    ejemplos.p[pos]++;
+                } else {
+                    ejemplos.n[pos]++;
+                }
             }
             //Calculamos las p, n, a, r...
             ejemplos.ajusta();
             tmpMerito = ejemplos.merito;
             if (tmpMerito < minMerito) {
-                minMerito=tmpMerito;
-                candidato=listAtributos.get(i);
+                minMerito = tmpMerito;
+                candidato = listAtributos.get(i);
+                System.out.println(candidato);
                 id3data = ejemplos;
+                id3data.atributo = candidato;
             }
         }
-        return candidato;
+        return id3data;
     }
-    
+
     public ArrayList<String> ejemplosRestantes(String valor, ArrayList<String> ejemplos) {
         ArrayList<String> retorno = new ArrayList();
+        String[] frase;
+        String recon = "";
+        boolean encontrado = false;
+        int j = 0;
         for (int i = 0; i < ejemplos.size(); i++) {
-            
+            frase = ejemplos.get(i).split(",");
+            for (int k = 0; k < frase.length; k++) {
+                if (!frase[k].equalsIgnoreCase(valor) && k < frase.length - 1) {
+                    recon += frase[k] + ",";
+                } else if (!frase[k].equalsIgnoreCase(valor) && k == frase.length - 1) {
+                    recon += frase[k];
+                }
+            }
+            retorno.add(recon);
+            System.out.println(recon);
+            recon = "";
         }
-        
+
         return retorno;
     }
-    
+
     public ArrayList<String> atributosRestantes(String valor, ArrayList<String> atributos) {
-        ArrayList<String> retorno = new ArrayList();
+        ArrayList<String> retorno = atributos;
+        int pos = -1;
         for (int i = 0; i < atributos.size(); i++) {
-            
-        }
-        
-        return retorno;
-    }
-    
-    public Vertice exec(ArrayList<String> listEjemplos, ArrayList<String> listAtributos, Vertice arbol) {
-        if (listEjemplos.isEmpty()) return null;
-        
-        if (isItPlusOrMinus(listEjemplos) == 1) {
-            arbol.setNodo(new Nodo("+"));
-        } else if (isItPlusOrMinus(listEjemplos) == -1) {
-            arbol.setNodo(new Nodo("-"));
-        } else {
-            if (listAtributos.isEmpty()) return null;
-            String mejor = minMerito(listEjemplos, listAtributos);
-            arbol.setNodo(new Nodo(mejor));
-            for (int i = 0; i < id3data.nombre.size(); i++) {
-                
+            if (atributos.get(i).equalsIgnoreCase(valor)) {
+                pos = i;
             }
         }
-        
-        return arbol;
+        if (pos != -1) {
+            retorno.remove(pos);
+        }
+        return retorno;
+    }
+
+    public void exec(ArrayList<String> listEjemplos, ArrayList<String> listAtributos) {
+        Nodo arbol = new Nodo();
+        arbol = execRec(listEjemplos, listAtributos, "");
+        System.out.println("Done!");
+    }
+    
+    public Nodo execRec(ArrayList<String> listEjemplos, ArrayList<String> listAtributos, String atributo) {
+        if (listEjemplos.isEmpty()) {
+            return null;
+        }
+        if (isItPlusOrMinus(listEjemplos) == 1) {
+            return new Nodo("+");
+        } else if (isItPlusOrMinus(listEjemplos) == -1) {
+            return new Nodo("-");
+        } else {
+            if (listAtributos.isEmpty()) {
+                return null;
+            }
+            id3data = minMerito(listEjemplos, listAtributos);
+            Nodo tmp = new Nodo();
+            for (int i = 0; i < id3data.nombre.size(); i++) {
+                tmp.hijos.add(execRec(ejemplosRestantes(id3data.nombre.get(i), listEjemplos),
+                        atributosRestantes(id3data.atributo, listAtributos)
+                        , id3data.atributo));
+            }
+            return tmp;
+        }
     }
 
 }
--- Pr2/PrsIC2/src/main/Main.java
+++ Pr2/PrsIC2/src/main/Main.java
@@ -6,6 +6,15 @@
 
 package main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author DanSirak
@@ -15,8 +24,35 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        File ejemploFile= new File("Juego.txt");
+        File atributoFile = new File("AtributosJuego.txt");
+        
+        ArrayList<String> atributoList = new ArrayList();
+        ArrayList<String> ejemploList = new ArrayList();
+        String fraseCompleta;
+        String[] frase;
+        try {
+            FileReader fr1 = new FileReader(atributoFile);
+            BufferedReader br1 = new BufferedReader(fr1);
+            fraseCompleta = br1.readLine();
+            frase = fraseCompleta.split(",");
+            for (int i = 0; i < frase.length; i++) atributoList.add(frase[i]);
+            fr1.close();
+            FileReader fr2 = new FileReader(ejemploFile);
+            BufferedReader br2 = new BufferedReader(fr2);
+            while((fraseCompleta = br2.readLine())!= null) {
+                ejemploList.add(fraseCompleta);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            Id3 id3 = new Id3(ejemploList, atributoList);
+            id3.exec(ejemploList, atributoList);
+        }
+
     }
     
 }
